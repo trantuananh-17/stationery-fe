65e126dc9a7ec904bdabad3d5e857f7f09302dc5
trantuananh-17 <anhkyohauik17@gmail.com>
2026-04-13 10:58:34 +0700

    feat: initial commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+import Footer from '@/components/Footer/footer';
+import Header from '@/components/Header/header';
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
